eba0a26ccfdbbe5591cfbaf60c78ef344da73f12
Mahmoud Allam <eng.ma.mo.allam@hotmail.com>
2022-07-18 09:19:01 +0200

    Add project files.

---
+++ HotelListing/Program.cs
@@ -0,0 +1,38 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//new
+builder.Host.UseSerilog((ctx, lc) => lc
+    .WriteTo.Console());
+builder.Services.AddCors(O=> 
+{
+    O.AddPolicy("AllowALL", build =>
+    build.AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors("AllowAll");
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
